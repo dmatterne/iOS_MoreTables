bef844fd63b95cd9cbf85df1b6662968fb4936b7
David Matterne <david.matterne@gmail.com>
2016-10-04 11:45:19 +0200

    Added delete

---
+++ MoreTables/DeleteCellsTableViewController.swift
@@ -0,0 +1,129 @@
+//
+//  DeleteCellsTableViewController.swift
+//  MoreTables
+//
+//  Created by Stannis Baratheon on 04/10/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import UIKit
+
+class DeleteCellsTableViewController: UITableViewController {
+
+    
+    var myList: [String]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let path = Bundle.main.path(forResource: "computers", ofType: "plist")
+        myList = NSArray(contentsOfFile: path!) as! [String]
+        
+        let editButton = UIBarButtonItem(title: "Delete", style: .plain, target: self, action: #selector(DeleteCellsTableViewController.toggleEdit(_ :)))
+        navigationItem.rightBarButtonItem = editButton
+        
+    }
+    
+    @IBAction func toggleEdit(_ sender: AnyObject) {
+    
+        tableView.setEditing(!tableView.isEditing, animated: true)
+        
+        if tableView.isEditing {
+        
+            navigationItem.rightBarButtonItem?.title = "Done"
+            
+        } else {
+            
+            navigationItem.rightBarButtonItem?.title = "Delete"
+        
+        
+        }
+        
+        
+        
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return myList.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "deleteCell", for: indexPath)
+
+        cell.textLabel?.text = myList[indexPath.row]
+        
+        
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            myList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+ 
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
